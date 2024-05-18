ec0f3b6baa67f9fa56a2b1542315f9d56c2f819a
AZV <arthur.dobromir@gmail.com>
2024-05-18 15:07:08 +0200

    v0.6.1x

---
+++ Games/BananaEats.lua
@@ -0,0 +1,12 @@
+local module = {}
+
+function module.PreInit()
+	_G.MX_SETTINGS.ESP.Mode = 1
+end
+
+function module.Init(category, connections)
+	local plr = game.Players.LocalPlayer
+	
+end
+
+return module
